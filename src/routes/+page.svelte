1b89660aa5106e43f79460c0a0167a06fdc3c465
Debarchito Nath <debarchiton@proton.me>
2025-12-18 17:48:39 +0530

    feat(shadcn): add shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<h1>Getting ready!</h1>
+<script>
+  import { Button } from "$lib/components/ui/button";
+  import * as Lucide from "@lucide/svelte";
+</script>
+
+<Button class="hover:shadow-4xl m-20 rounded-4xl hover:shadow active:rounded-sm">
+  <Lucide.Home />
+</Button>
